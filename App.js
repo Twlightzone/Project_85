5081117fa685d55e76d0f8cc012a6aa857b848ff
Twlight_zone <twlightzonehelp@gmail.com>
2022-07-27 17:42:28 +0530

    Did some things

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { createSwitchNavigator, creatAppContainer } from 'react-navigation';
+
+import LoadingScreen from './screens/LoadingScreen';
+import LoginScreen from './screens/LoginScreen';
+import DashboardScreen from './screens/DashboardScreen';
+
+import * as firebase from 'firebase';
+import { firebaseconfig } from './config';
+
+const AppSwitchNavigator = createSwitchNavigator({
+  LoadingScreen : LoadingScreen,
+  LoginScreen : LoginScreen,
+  DashboardScreen : DashboardScreen
+})
+
+const AppNavigator = creatAppContainer(AppSwitchNavigator)
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return(
+    <AppNavigator/>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/LoadingScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class LoadingScreen extends Component {
+    render(){
+        return(
+            <View>
+                <Text>
+                    LoadingScreen
+                </Text>
+            </View>
+        )
+    }
+}
