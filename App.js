5aac62e93c3e609fa1676de78e020507d7a0ace8
Twlight_zone <twlightzonehelp@gmail.com>
2022-07-27 17:55:28 +0530

    Commited to LoadingScreen

--- App.js
+++ App.js
@@ -8,6 +8,12 @@ import DashboardScreen from './screens/DashboardScreen';
 import * as firebase from 'firebase';
 import { firebaseconfig } from './config';
 
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseconfig);
+} else {
+  firebase.app();
+}
+
 const AppSwitchNavigator = createSwitchNavigator({
   LoadingScreen : LoadingScreen,
   LoginScreen : LoginScreen,
--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
+
+import firebase from 'firebase';
 
 export default class LoadingScreen extends Component {
+    componentDidMount() {
+        this.checkIfLoggedIn();
+    }
+
+    checkIfLoggedIn = () => {
+        firebase.auth().onAuthStateChanged((user)=>{
+            if (user) {
+                this.props.navigation.navigate('DashboardScreen')
+            } else {
+                this.props.navigation.navigate('LoginScreen')
+            }
+        })
+    }
+
     render(){
         return(
-            <View>
-                <Text>
-                    LoadingScreen
-                </Text>
+            <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                <ActivityIndicator size='large'/>
             </View>
         )
     }
